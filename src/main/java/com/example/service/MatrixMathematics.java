9259f185d23df388e9beace8d2a79cb143d7a275
mossab12 <32892512+mossab12@users.noreply.github.com>
2019-01-14 11:36:39 +0100

    Update MatrixMathematics.java

--- src/main/java/com/example/service/MatrixMathematics.java
+++ src/main/java/com/example/service/MatrixMathematics.java
@@ -30,7 +30,7 @@ public class MatrixMathematics {
 	 * @throws NoSquareException
 	 */
 	public static Matrix inverse(Matrix matrix) throws NoSquareException {
-		return 0;
+		return (transpose(cofactor(matrix)).multiplyByConstant(1.0/determinant(matrix)));
 	}
 	
 	
