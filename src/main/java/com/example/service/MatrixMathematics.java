6a67b21dd4c9cdc397c2e7e0d534fa8831b21c72
mossab12 <mossabinfo@gmail.com>
2019-01-14 11:32:19 +0100

    fix

--- src/main/java/com/example/service/MatrixMathematics.java
+++ src/main/java/com/example/service/MatrixMathematics.java
@@ -6,91 +6,6 @@ import com.example.model.Matrix;
 public class MatrixMathematics {
 
 	/**
-	 * This class a matrix utility class and cannot be instantiated.
-	 */
-	private MatrixMathematics(){}
-
-
-	/**
-	 * Determinant of a square matrix
-	 * The following function find the determinant in a recursively.
-	 * @param matrix
-	 * @return
-	 * @throws NoSquareException
-	 */
-	public static double determinant(Matrix matrix) throws NoSquareException {
-		if (!matrix.isSquare())
-			throw new NoSquareException("matrix need to be square.");
-		if (matrix.size() == 1){
-			return matrix.getValueAt(0, 0);
-		}
-
-		if (matrix.size()==2) {
-			return (matrix.getValueAt(0, 0) * matrix.getValueAt(1, 1)) - ( matrix.getValueAt(0, 1) * matrix.getValueAt(1, 0));
-		}
-		double sum = 0.0;
-		for (int i=0; i<matrix.getNcols(); i++) {
-			sum += changeSign(i) * matrix.getValueAt(0, i) * determinant(createSubMatrix(matrix, 0, i));
-		}
-		return sum;
-	}
-
-	/**
-	 * Determine the sign; i.e. even numbers have sign + and odds -
-	 * @param i
-	 * @return
-	 */
-	private static int changeSign(int i) {
-		if (i%2==0)
-			return 1;
-		return -1;
-	}
-	/**
-	 * Creates a submatrix excluding the given row and column
-	 * @param matrix
-	 * @param excludingrow
-	 * @param excludingcol
-	 * @return
-	 */
-	public static Matrix createSubMatrix(Matrix matrix, int excludingrow, int excludingcol) {
-
-		Matrix mat = new Matrix(matrix.getNrows()-1, matrix.getNcols()-1);
-		int r = -1;
-		for (int i=0;i<matrix.getNrows();i++) {
-			if (i==excludingrow) {
-				continue;
-			}
-				r++;
-				int c = -1;
-			for (int j=0;j<matrix.getNcols();j++) {
-				if (j==excludingcol) {
-					continue;
-				}
-				mat.setValueAt(r, ++c, matrix.getValueAt(i, j));
-			}
-		}
-		return mat;
-	}
-
-	/**
-	 * The cofactor of a matrix
-	 * @param matrix
-	 * @return
-	 * @throws NoSquareException
-	 */
-	public static Matrix cofactor(Matrix matrix) throws NoSquareException {
-		Matrix mat = new Matrix(matrix.getNrows(), matrix.getNcols());
-		for (int i=0;i<matrix.getNrows();i++) {
-			for (int j=0; j<matrix.getNcols();j++) {
-				mat.setValueAt(i, j, changeSign(i) * changeSign(j) * determinant(createSubMatrix(matrix, i, j)));
-			}
-		}
-
-		return mat;
-	}
-
-
-	/**
 	 * Transpose of a matrix - Swap the columns with rows
 	 * @param matrix
 	 * @return
@@ -104,7 +19,7 @@ public class MatrixMathematics {
 		}
 		return transposedMatrix;
 	}
-
+	
 	/**
 	 * Inverse of a matrix - A-1 * A = I where I is the identity matrix
 	 * A matrix that have inverse is called non-singular or invertible. If the matrix does not have inverse it is called singular.
@@ -117,6 +32,10 @@ public class MatrixMathematics {
 	public static Matrix inverse(Matrix matrix) throws NoSquareException {
 		return (transpose(cofactor(matrix)).multiplyByConstant(1.0/determinant(matrix)));
 	}
+	
+	
 
 
+	
+	
 }
